d50910e0e1bedb14e277bcd0ee6c3cd601ad419f
latzyx <1360561224@qq.com>
2022-06-27 20:56:27 +0800

    end

--- gee/gee.go
+++ gee/gee.go
@@ -1,27 +1,26 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // HandlerFunc 创建http.handlerfunc
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+// "HandlerFunc" defines the request handler used by gee
+type HandlerFunc func(*Context)
 
-// Engine  创建一个router结构体
+// Engine  创建一个router结构体 implement the interface of ServeHTTP
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 // New 新建一个router
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 // addRoute 添加一个route :method 传递方法，pattern 模式,handler 方法
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+	engine.router.addRoute(method, pattern, handler)
 }
 
 // GET 创建一个GET方法,传递参数 :pattern 模式， handler 方法
@@ -39,11 +38,6 @@ func (engine *Engine) Run(addr string) (err error) {
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
